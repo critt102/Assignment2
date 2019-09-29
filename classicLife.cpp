b9fd565f1bc14027cfe113f5552cc1ed1f1bd725
Mary Kate Crittenden <critt102@mail.chapman.edu>
2019-09-29 05:18:37 +0000

    almost done

--- classicLife.cpp
+++ classicLife.cpp
@@ -16,10 +16,13 @@ ClassicLife::ClassicLife(){
   enter=false;
   sim=0;
   test_equality=false;
+  rand_dec=0;
+  outputfile='n';
+  out_name="";
+  gen=0;
   inArray=new char[numvar];
   lifeArray= new char*[width];
   newArray=new char*[width];
-  //ifstream in_file;
   for(int k = 0; k < width; ++k){
     lifeArray[k] = new char[height];
     newArray[k] = new char[height];
@@ -32,18 +35,32 @@ ClassicLife::ClassicLife(string filename){
   cell='-';
   thiscell=false;
   sim=0;
+  gen=0;
   test_equality=false;
-  //ifstream in_file;
-  cout<<"Do you want to press the enter key between generations? y or n? ";
-  cin>>enter_in;
-  enter_in=tolower(enter_in);
-  if(enter_in=='y'){
-    enter=true;
+  rand_dec=0;
+  in_file.open(filename, ios::in);
+  if(in_file.is_open()==false){
+    cout<<"File "<<filename<<" not found."<<endl;
+    exit(0);
+  }
+  cout<<"Do you want to output results to a file? y or n? "<<endl;
+  cin>>outputfile;
+  if (outputfile=='y' || outputfile== 'Y'){
+    cout<<"Enter the name of the file you wish to output to, in the format filename.txt "<<endl;
+    cin>>out_name;
+    out_file.open(out_name, ios::out);
   }
   else{
-    enter=false;
+    cout<<"Do you want to press the enter key between generations? y or n? "<<endl;
+    cin>>enter_in;
+    enter_in=tolower(enter_in);
+    if(enter_in=='y'){
+      enter=true;
+    }
+    else{
+      enter=false;
+    }
   }
-  in_file.open(filename, ios::in);
   if(in_file.is_open()){
     //counting chars
     while(getline(in_file, line)){
@@ -76,9 +93,75 @@ ClassicLife::ClassicLife(string filename){
   for(int row=0; row<height; ++row){
     for(int col=0; col<width; ++col){
       lifeArray[row][col]=inArray[currentvalue++];
-      // cout<<"row "<<row;
-      // cout<<" col "<<col<<" ";
-      // cout<<inArray[currentvalue++]<<endl;
+    }
+  }
+}
+
+ClassicLife::ClassicLife(int r_height, int r_width, double r_decimal){
+  rand_dec=r_decimal;
+  outputfile='n';
+  out_name="";
+  height=r_height;
+  width=r_width;
+  numvar=1;
+  gen=0;
+  loopvalue=0;
+  currentvalue=2;
+  cell='-';
+  thiscell=false;
+  sim=0;
+  test_equality=false;
+  inArray=new char[numvar];
+  cout<<"Do you want to output results to a file? y or n? "<<endl;
+  cin>>outputfile;
+  if (outputfile=='y' || outputfile== 'Y'){
+    cout<<"Enter the name of the file you wish to output to, in the format filename.txt "<<endl;
+    cin>>out_name;
+    out_file.open(out_name, ios::out);
+  }
+  else{
+    cout<<"Do you want to press the enter key between generations? y or n? "<<endl;
+    cin>>enter_in;
+    enter_in=tolower(enter_in);
+    if(enter_in=='y'){
+      enter=true;
+    }
+    else{
+      enter=false;
+    }
+  }
+
+  //create 2d array
+  lifeArray= new char*[width];
+  newArray=new char*[width];
+  for(int k = 0; k < width; ++k){
+    lifeArray[k] = new char[height];
+    newArray[k] = new char[height];
+  }
+  //fill array with '-'
+  for(int f=0; f<height; ++f){
+    for(int g=0; g<width; ++g){
+      lifeArray[f][g]='-';
+    }
+  }
+  //fill with 'X' according to density calculation
+  double density= (width*height)*rand_dec;
+  while(density>0){
+    srand (time(NULL));
+    for(int q=0; q<height; ++q){
+      if(density==0){
+        break;
+      }
+      for(int r=0; r<width; ++r){
+        if(density==0){
+          break;
+        }
+        int num_a=rand()%2;
+        if(num_a==1){
+          lifeArray[q][r]='X';
+          density--;
+        }
+      }
     }
   }
 }
@@ -88,14 +171,6 @@ ClassicLife::~ClassicLife(){
   delete newArray;
   delete inArray;
 }
-// bool testEquality(){
-//   if(lifeArray==newArray){
-//     return true;
-//   }
-//   else{
-//     return false;
-//   }
-// }
 bool ClassicLife::fullCell(char x){
   if (x=='X'){
     return true;
@@ -107,12 +182,41 @@ bool ClassicLife::fullCell(char x){
     cout<<"ERROR"<<endl;
   }
 }
-void ClassicLife::writeToFile(string out_name){
-  cout<<"Write to file"<<endl;
-}
+
 void ClassicLife::runClassic(){
   if(enter==true){
     getchar();
+  }
+  if(out_file.is_open())
+    out_file<<gen<<endl;
+  else
+    cout<<gen<<endl;
+  for(int row=0; row<height; ++row){
+    for(int col=0; col<width; ++col){
+      if(out_file.is_open())
+        out_file<<lifeArray[row][col];
+      else
+        cout<<lifeArray[row][col];
+    }
+    if(out_file.is_open())
+      out_file<<endl;
+    else
+      cout<<endl;
+  }
+  gen++;
+  if(out_file.is_open())
+    out_file<<endl;
+  else{
+    if(enter==true){
+      cout<<"Press Enter to continue.";
+      getchar();
+      cout<<endl;
+    }
+    else{
+      cout<<endl;
+      chrono::seconds time(2);
+      this_thread::sleep_for(time);
+    }
   }
   while (test_equality==false){
     //overall map rows
@@ -126,7 +230,8 @@ void ClassicLife::runClassic(){
         thiscell=fullCell(cell);
         for(int a=(row_c-1); a<=(row_c+1); a++){
           for(int b=(col_c-1); b<=(col_c+1); b++){
-            if(a>=0 && b>=0 && a<=4 && b<=6){
+            if(a>=0 && b>=0 && a<height && b<width){
+              //  if(c>=0 &&d>=0 && c<height && d<width)
               thiscell=fullCell(lifeArray[a][b]);
             }
             else{
@@ -159,12 +264,27 @@ void ClassicLife::runClassic(){
         }
       }
     }
+
+    if(out_file.is_open())
+      out_file<<gen<<endl;
+    else
+      cout<<gen<<endl;
     for(int row_new=0; row_new<height; ++row_new){
       for(int col_new=0; col_new<width; ++col_new){
-        cout<<newArray[row_new][col_new];
+        if(out_file.is_open())
+          out_file<<newArray[row_new][col_new];
+        else
+          cout<<newArray[row_new][col_new];
       }
-      cout<<endl;
+
+      if(out_file.is_open())
+        out_file<<endl;
+      else
+        cout<<endl;
     }
+    gen++;
+    if(out_file.is_open())
+      out_file<<endl;
     test_equality=true;
     for(int equal=0; equal<height; ++equal){
       for(int ity=0; ity<width; ++ity){
@@ -173,18 +293,24 @@ void ClassicLife::runClassic(){
         }
       }
     }
-    if(enter==true){
-      cout<<"Press Enter to continue.";
-      getchar();
-    }
-    else{
-      chrono::seconds time(2);
-      this_thread::sleep_for(time);
+    if(out_file.is_open()==false){
+      if(enter==true){
+        cout<<"Press Enter to continue.";
+        getchar();
+      }
+      else{
+        chrono::seconds time(2);
+        this_thread::sleep_for(time);
+      }
     }
     if (test_equality==true)
       cout<<"World has stabilized"<<endl;
+      if(out_file.is_open() && test_equality==true){
+        cout<<"Output has been written to file "<<out_name<<endl;
+      }
     else if(test_equality==false){
-      cout<<endl;
+      if(out_file.is_open()==false)
+        cout<<endl;
     }
     if(lifeArray!=newArray){
       for(int copy_h=0; copy_h<height; ++copy_h){
@@ -194,11 +320,45 @@ void ClassicLife::runClassic(){
       }
     }
   }
+  if(out_file.is_open())
+    out_file.close();
 }
 
 void ClassicLife::runDoughnut(){
   if(enter==true){
     getchar();
+  }
+
+  if(out_file.is_open())
+    out_file<<gen<<endl;
+  else
+    cout<<gen<<endl;
+  for(int row=0; row<height; ++row){
+    for(int col=0; col<width; ++col){
+      if(out_file.is_open())
+        out_file<<lifeArray[row][col];
+      else
+        cout<<lifeArray[row][col];
+    }
+    if(out_file.is_open())
+      out_file<<endl;
+    else
+      cout<<endl;
+  }
+  gen++;
+  if(out_file.is_open())
+    out_file<<endl;
+  else{
+    if(enter==true){
+      cout<<"Press Enter to continue.";
+      getchar();
+      cout<<endl;
+    }
+    else{
+      cout<<endl;
+      chrono::seconds time(2);
+      this_thread::sleep_for(time);
+    }
   }
   while (test_equality==false){
     //overall map rows
@@ -215,11 +375,7 @@ void ClassicLife::runDoughnut(){
           for(int d=(col_d-1); d<=(col_d+1); d++){
             if(c>=0 &&d>=0 && c<height && d<width){
               thiscell=fullCell(lifeArray[c][d]);
-
             }
-            // else{
-            //   thiscell=false;
-            // }
             else if(c<0 && d<width){
               if(d<0)
                 thiscell=fullCell(lifeArray[height-1][width-1]);
@@ -273,12 +429,26 @@ void ClassicLife::runDoughnut(){
         }
       }
     }
+
+    if(out_file.is_open())
+      out_file<<gen<<endl;
+    else
+      cout<<gen<<endl;
     for(int row_new=0; row_new<height; ++row_new){
       for(int col_new=0; col_new<width; ++col_new){
-        cout<<newArray[row_new][col_new];
+        if(out_file.is_open())
+          out_file<<newArray[row_new][col_new];
+        else
+          cout<<newArray[row_new][col_new];
       }
-      cout<<endl;
+      if(out_file.is_open())
+        out_file<<endl;
+      else
+        cout<<endl;
     }
+    gen++;
+    if(out_file.is_open())
+      out_file<<endl;
     test_equality=true;
     for(int equal=0; equal<height; ++equal){
       for(int ity=0; ity<width; ++ity){
@@ -287,18 +457,24 @@ void ClassicLife::runDoughnut(){
         }
       }
     }
-    if(enter==true){
-      cout<<"Press Enter to continue.";
-      getchar();
-    }
-    else{
-      chrono::seconds time(2);
-      this_thread::sleep_for(time);
+    if(out_file.is_open()==false){
+      if(enter==true){
+        cout<<"Press Enter to continue.";
+        getchar();
+      }
+      else{
+        chrono::seconds time(2);
+        this_thread::sleep_for(time);
+      }
     }
     if (test_equality==true)
       cout<<"World has stabilized"<<endl;
+      if(out_file.is_open()&& test_equality==true){
+        cout<<"Output has been written to file "<<out_name<<endl;
+      }
     else if(test_equality==false){
-      cout<<endl;
+      if(out_file.is_open()==false)
+        cout<<endl;
     }
     if(lifeArray!=newArray){
       for(int copy_h=0; copy_h<height; ++copy_h){
@@ -308,6 +484,170 @@ void ClassicLife::runDoughnut(){
       }
     }
   }
+  if(out_file.is_open())
+    out_file.close();
+}
+
+void ClassicLife::runMirror(){
+  if(enter==true){
+    getchar();
+  }
+  if(out_file.is_open())
+    out_file<<gen<<endl;
+  else
+    cout<<gen<<endl;
+  for(int row=0; row<height; ++row){
+    for(int col=0; col<width; ++col){
+      if(out_file.is_open())
+        out_file<<lifeArray[row][col];
+      else
+        cout<<lifeArray[row][col];
+    }
+    if(out_file.is_open())
+      out_file<<endl;
+    else
+      cout<<endl;
+  }
+  gen++;
+  if(out_file.is_open())
+    out_file<<endl;
+  else{
+    if(enter==true){
+      cout<<"Press Enter to continue.";
+      getchar();
+      cout<<endl;
+    }
+    else{
+      cout<<endl;
+      chrono::seconds time(2);
+      this_thread::sleep_for(time);
+    }
+  }
+  while (test_equality==false){
+    //overall map rows
+    for(int row_m=0; row_m<height; row_m++){
+      //overall maps columns
+      for(int col_m=0; col_m<width; col_m++){
+        //cell we are focusing on
+        cell=lifeArray[row_m][col_m];
+        sim=0;
+        //check if current cell is full
+        thiscell=fullCell(cell);
+        //cout<<"row_d "<<row_d<<" col_d "<<col_d<<endl;
+        for(int c=(row_m-1); c<=(row_m+1); c++){
+          for(int d=(col_m-1); d<=(col_m+1); d++){
+            if(c>=0 &&d>=0 && c<height && d<width){
+              thiscell=fullCell(lifeArray[c][d]);
+            }
+            else if(c<0 && d<width){
+              if(d<0)
+                thiscell=fullCell(lifeArray[c+1][d+1]);
+              else
+                thiscell=fullCell(lifeArray[c+1][d]);
+            }
+            else if(c>=height && d>=0){
+              if(d>=width)
+                thiscell=fullCell(lifeArray[c-1][d-1]);
+              else
+                thiscell=fullCell(lifeArray[c-1][d]);
+            }
+            else if(d<0 && c>=0){
+              if(c>=height)
+                thiscell=fullCell(lifeArray[c-1][d+1]);
+              else
+                thiscell=fullCell(lifeArray[c][d+1]);
+            }
+            else if(d>=width && c<height){
+              if(c<0)
+                thiscell=fullCell(lifeArray[c+1][d-1]);
+              else
+                thiscell=fullCell(lifeArray[c][d-1]);
+            }
+            if(thiscell==true){
+              sim++;
+            }
+            else{
+            }
+          }
+        }
+        if (cell=='X'){
+          sim--;
+        }
+        if(sim<=1){
+          newArray[row_m][col_m]='-';
+        }
+        else if(sim==2){
+          if (cell=='X'){
+            newArray[row_m][col_m]='X';
+          }
+          else{
+            newArray[row_m][col_m]='-';
+          }
+        }
+        else if(sim==3){
+          newArray[row_m][col_m]='X';
+        }
+        else if(sim>=4){
+          newArray[row_m][col_m]='-';
+        }
+      }
+    }
+    if(out_file.is_open())
+      out_file<<gen<<endl;
+    else
+      cout<<gen<<endl;
+    for(int row_new=0; row_new<height; ++row_new){
+      for(int col_new=0; col_new<width; ++col_new){
+        if(out_file.is_open())
+          out_file<<newArray[row_new][col_new];
+        else
+          cout<<newArray[row_new][col_new];
+      }
+      if(out_file.is_open())
+        out_file<<endl;
+      else
+        cout<<endl;
+    }
+    gen++;
+    if(out_file.is_open())
+      out_file<<endl;
+    test_equality=true;
+    for(int equal=0; equal<height; ++equal){
+      for(int ity=0; ity<width; ++ity){
+        if(lifeArray[equal][ity]!=newArray[equal][ity]){
+          test_equality=false;
+        }
+      }
+    }
+    if(out_file.is_open()==false){
+      if(enter==true){
+        cout<<"Press Enter to continue.";
+        getchar();
+      }
+      else{
+        chrono::seconds time(2);
+        this_thread::sleep_for(time);
+      }
+    }
+    if (test_equality==true)
+      cout<<"World has stabilized"<<endl;
+      if(out_file.is_open() && test_equality==true){
+        cout<<"Output has been written to file "<<out_name<<endl;
+      }
+    else if(test_equality==false){
+      if(out_file.is_open()==false)
+        cout<<endl;
+    }
+    if(lifeArray!=newArray){
+      for(int copy_h=0; copy_h<height; ++copy_h){
+        for(int copy_w=0; copy_w<width; ++copy_w){
+          lifeArray[copy_h][copy_w]=newArray[copy_h][copy_w];
+        }
+      }
+    }
+  }
+  if(out_file.is_open())
+    out_file.close();
 }
 
 bool ClassicLife::isEmpty(){
--- lifemain.cpp
+++ lifemain.cpp
@@ -1,12 +1,85 @@
 #include "classicLife.h"
 #include <iostream>
 #include <fstream>
+
 using namespace std;
 
 int main(int argc, char**argv){
-  string file_name=argv[1];
-  ClassicLife gameone(file_name);
-  //gameone.runClassic();
-  gameone.runDoughnut();
+  bool valid_in=false;
+  string config;
+  string mode;
+  bool input_o=false;
+
+  while(valid_in==false){
+    cout<<"Do you want provide a flat-file configuration, or start with a random configuration?"<<endl;
+    cout<<"file or random? "<<endl;
+    cin>>config;
+
+    if(config=="random" || config=="Random"){
+      valid_in=true;
+      double r_decimal;
+      int r_height;
+      int r_width;
+      cout<<"Enter the height of your desired grid size: ";
+      cin>>r_height;
+      cout<<"Enter the width of your desired grid size: ";
+      cin>>r_width;
+      cout<<"Enter a decimal value greater than 0 and less than or equal to 1: ";
+      cin>>r_decimal;
+      ClassicLife gameRand(r_height, r_width, r_decimal);
+      while(input_o==false){
+        cout<<"What boundry mode do you want to run in?"<<endl;
+        cout<<"classic, doughnut, or mirror?"<<endl;
+        cin>>mode;
+        if(mode=="classic" || mode=="Classic"){
+          gameRand.runClassic();
+          input_o=true;
+        }
+        else if(mode=="doughnut" || mode=="Doughnut"){
+          gameRand.runDoughnut();
+          input_o=true;
+        }
+        else if(mode=="mirror" || mode=="Mirror"){
+          gameRand.runMirror();
+          input_o=true;
+        }
+        else{
+          cout<<mode<<" is not a valid input."<<endl;
+        }
+      }
+    }
+    else if(config=="file" || config=="File"){
+      valid_in=true;
+      string file_name;
+      cout<<"Enter the name of the file you wish use, in format filename.txt"<<endl;
+      cin>>file_name;
+      ClassicLife gameOne(file_name);
+      while(input_o==false){
+        cout<<"What boundry mode do you want to run in?"<<endl;
+        cout<<"classic, doughnut, or mirror?"<<endl;
+        cin>>mode;
+        if(mode=="classic" || mode=="Classic"){
+          gameOne.runClassic();
+          input_o=true;
+        }
+        else if(mode=="doughnut" || mode=="Doughnut"){
+          gameOne.runDoughnut();
+          input_o=true;
+        }
+        else if(mode=="mirror" || mode=="Mirror"){
+          gameOne.runMirror();
+          input_o=true;
+        }
+        else{
+          cout<<mode<<" is not a valid input."<<endl;
+        }
+      }
+    }
+    else{
+      cout<<config<<" is not a valid input."<<endl;
+    }
+  }
+
+
   return 0;
 }
